3629250b69c1369a8195e88c236642adaad0400b
Vaibhav422 <92776886+Vaibhav992@users.noreply.github.com>
2024-03-01 01:51:37 +0530

    Building the final end point URL created

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
+const URL = process.env.NODE_ENV === 'production' ? 'https://realtime-drawing-tool-backend.onrender.com' : 'http://localhost:5000'
 
-export const socket = io('http://localhost:8000');
+
+export const socket = io(URL);
